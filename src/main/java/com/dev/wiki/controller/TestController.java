d4ca4ab93670291967bb55f626436ec0ef08b0fd
Dev <yh515511038@gmail.com>
2021-11-26 21:59:52 +0800

    2-9 集成热部署   1.添加sprint-boot-devtools依赖   2.IDEA Compiler设置 auto build project   3.registry... 中设置 automake when running

--- src/main/java/com/dev/wiki/controller/TestController.java
+++ src/main/java/com/dev/wiki/controller/TestController.java
@@ -1,32 +1,37 @@
 package com.dev.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController     // @Controller(返回页面) + @ResponseBody(返回字符串或JSON对象)
 
 public class TestController {
 
+    @Value("${test.hello:default}")     // 通过 @Value注解 获取自定义配置项(支持默认值)
+    private String testHello;
+
     /**
      * 常见的Http请求类型：
-     *  GET -> 查询
-     *  POST -> 新增
-     *  PUT -> 修改
-     *  DELETE -> 删除
-     *  传统类型：/user?id=1
-     *  REST方式：/user/1 配合以下几种注解：
-     *      @GetMapping
-     *      @PostMapping
-     *      @PutMapping
-     *      @DeleteMapping
-     *  @RequestMapping 支持所有请求方式，可通过method参数进行区分
+     * GET -> 查询
+     * POST -> 新增
+     * PUT -> 修改
+     * DELETE -> 删除
+     * 传统类型：/user?id=1
+     * REST方式：/user/1 配合以下几种注解：
+     *
      * @return
+     * @GetMapping
+     * @PostMapping
+     * @PutMapping
+     * @DeleteMapping
+     * @RequestMapping 支持所有请求方式，可通过method参数进行区分
      */
     // @RequestMapping(value = "/hello", method = RequestMethod.GET)
     // @RequestMapping("/hello")   // 定义接口对应的请求地址
     // @PostMapping("/hello")  // 只支持POST请求，提交GET请求会报405错误(type=Method Not Allowed, status=405)
     @GetMapping("/hello")   // 只支持GET请求
     public String hello() {
-        return "Hello World!";
+        return "Hello World!, " + testHello;
     }
 
     @PostMapping("/hello/post")
